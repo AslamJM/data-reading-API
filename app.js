e06009e867d715b122b5624693ff1e8c8de01885
AslamJM <jma8744@gmail.com>
2021-10-27 23:44:19 +0530

    port change

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ db.on("open", () => console.log("success"));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/reading", router);
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("server is listening");
 });
